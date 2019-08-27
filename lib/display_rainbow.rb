676af017ee07a020a4cc33d01c0890a4a8313e0f
Julian Wakefield <julianwakefield@outlook.com>
2019-08-27 12:37:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 def display_rainbow(colors)
   display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  colors = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
+  
 end
